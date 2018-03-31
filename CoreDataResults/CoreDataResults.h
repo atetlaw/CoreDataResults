80579483ca110711e15f2a282331fe513e4cad1a
Andrew Tetlaw <andrew@tetlaw.id.au>
2018-03-31 17:25:19 +0800

    Initial Commit

---
+++ CoreDataResults/CoreDataResults.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataResults.h
+//  CoreDataResults
+//
+//  Created by Andrew Tetlaw on 31/3/18.
+//  Copyright © 2018 Dexagogo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataResults.
+FOUNDATION_EXPORT double CoreDataResultsVersionNumber;
+
+//! Project version string for CoreDataResults.
+FOUNDATION_EXPORT const unsigned char CoreDataResultsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataResults/PublicHeader.h>
+
+
